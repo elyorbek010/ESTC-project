0bf81d3d478be99d56dc3b4e734cbdf8acf8538a
Elyorbek Kholikov <elyorbek4111020@list.ru>
2022-11-12 09:25:12 +0000

    pwm blinky added

--- gpio_hal/main.c
+++ gpio_hal/main.c
@@ -15,25 +15,25 @@
 #include "nrf_gpio.h"
 #include "boards.h"
 
-#define led1 8
-#define led2 NRF_GPIO_PIN_MAP(1, 9)
-#define led3 12
+#define led_R 8
+#define led_G NRF_GPIO_PIN_MAP(1, 9)
+#define led_B 12
 #define button NRF_GPIO_PIN_MAP(1, 6)
 /**
  * @brief Function for application main entry.
  */
 int main(void)
 {
-    nrf_gpio_cfg_input(button, NRF_GPIO_PIN_PULLUP); // Configure P1.6(SW1) as pull-up input
-    nrf_gpio_cfg_output(led1);   // Configure P0.8(LED2_R) as output
-    nrf_gpio_cfg_output(led2);   // Configure P1.9(LED2_G) as output
-    nrf_gpio_cfg_output(led3);   // Configure P0.12(LED2_B) as output
+    nrf_gpio_cfg_input(button, NRF_GPIO_PIN_PULLDOWN); // Configure P1.6(SW1) as pull-up input
+    nrf_gpio_cfg_output(led_R);   // Configure P0.8(LED2_R) as output
+    nrf_gpio_cfg_output(led_G);   // Configure P1.9(LED2_G) as output
+    nrf_gpio_cfg_output(led_B);   // Configure P0.12(LED2_B) as output
 
-    nrf_gpio_pin_write(led1, 1);
-    nrf_gpio_pin_write(led2, 1);
-    nrf_gpio_pin_write(led3, 1);
+    nrf_gpio_pin_write(led_R, 1);
+    nrf_gpio_pin_write(led_G, 1);
+    nrf_gpio_pin_write(led_B, 1);
 
-    int cnt = 0, arr[] = {led1, led1, led2, led2, led2, led3, led3}; // RRGGGBB
+    int cnt = 0, arr[] = {led_R, led_R, led_G, led_G, led_G, led_B, led_B}; // RRGGGBB
     int size = sizeof(arr) / sizeof(arr[0]);
 
     while(true){
@@ -41,8 +41,8 @@ int main(void)
             nrf_gpio_pin_write(arr[cnt], 0);
             nrf_delay_ms(500);
             nrf_gpio_pin_write(arr[cnt], 1);
-            cnt++;
             nrf_delay_ms(500);
+            cnt++;
         }
         if(cnt == size) {
             cnt = 0;
